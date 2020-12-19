9318259e5c896888175e957b52a11277bc4d61a2
auooru <auooru@outlook.com>
2020-12-20 00:04:06 +0800

    update: hash.js - hmac

--- src/crypto/hash.js
+++ src/crypto/hash.js
@@ -3,6 +3,8 @@ import md5 from "node-forge/lib/md5.js";
 import sha1 from "node-forge/lib/sha1.js";
 import sha256 from "node-forge/lib/sha256.js";
 import sha512 from "node-forge/lib/sha512.js";
+import hmac from "node-forge/lib/hmac.js";
+import util from "node-forge/lib/util.js";
 
 function hash_wrapper(algoMd, data, rawOutput = false)
 {
@@ -40,6 +42,21 @@ export function hash_sha256(data, rawOutput = false)
 export function hash_sha512(data, rawOutput = false)
 {
   return hash_wrapper(sha512, data, rawOutput)
+}
+
+/**
+ * @param {String} algo
+ * @param {String} str
+ * @param {String} key
+ * @param {Boolean} rawOutput
+ * @returns {String}
+ */
+export function hash_hmac (algo, str, key, rawOutput = false) {
+  const h = hmac.create()
+  h.start(algo, util.encodeUtf8(key, 'utf8'))
+  h.update(util.encodeUtf8(str, 'utf8'))
+  const result = h.digest()
+  return rawOutput ? result.getBytes() : result.toHex()
 }
 
 export default {
--- test/crypto.test.js
+++ test/crypto.test.js
@@ -1,5 +1,5 @@
 import {base64_decode, base64_encode, random_bytes, random_hex} from '../src/crypto/util.js';
-import {hash, hash_md5, hash_sha1, hash_sha256, hash_sha512} from '../src/crypto/hash.js';
+import {hash, hash_md5, hash_sha1, hash_sha256, hash_sha512, hash_hmac} from '../src/crypto/hash.js';
 import {aes_gcm_encrypt, aes_gcm_decrypt} from '../src/crypto/aes.js';
 import test from "ava";
 
@@ -49,6 +49,16 @@ test('hash_sha512', t => {
   const text = '123456qweasd请问阿斯顿';
 
   t.is(hash_sha512(text), '3f9556e4b888837a78112f6aac41f093666aa42631232308b31bcaf9fcc6f3137fda8e213065a9223a0992bdffe3ad13ba6737975f954e30ebfe48554e318516')
+})
+
+test('hash_hmac', t => {
+  const text = '123456qweasd请问阿斯顿';
+  const key = '123456789啊';
+
+  t.is(hash_hmac('md5', text, key), 'cca3faa5169449ec2eafc3a31a02539c')
+  t.is(hash_hmac('sha1', text, key), '27fa3d911164b3450ef489189cc65d4826df1b4f')
+  t.is(hash_hmac('sha256', text, key), 'a7bcd496124504f976c357d315c36000fb533ef6a732e12893179bbcb50b1e11')
+  t.is(hash_hmac('sha512', text, key), 'ec9911c0a1c25eba781cd4f959b7204b8e06a1386345a36666ab799ef948bdb352a9f9649d9cbfa3080fd3f8af3f5907e0e2df05081996dcf7a1537c71799587')
 })
 
 test('aes_gcm', t => {

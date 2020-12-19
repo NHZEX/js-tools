fecf574a5a8687d0cfe60383c8998df5cdad789d
auooru <auooru@outlook.com>
2020-12-19 23:42:14 +0800

    update: hash.js

--- src/crypto/hash.js
+++ src/crypto/hash.js
@@ -1,4 +1,16 @@
 import md from "node-forge/lib/md.all.js";
+import md5 from "node-forge/lib/md5.js";
+import sha1 from "node-forge/lib/sha1.js";
+import sha256 from "node-forge/lib/sha256.js";
+import sha512 from "node-forge/lib/sha512.js";
+
+function hash_wrapper(algoMd, data, rawOutput = false)
+{
+  const hash = algoMd.create()
+  hash.update(data, 'utf8')
+  const result = hash.digest()
+  return rawOutput ? result.getBytes() : result.toHex()
+}
 
 /**
  * @param {String} algo md5, sha1 sha256 sha384 sha512
@@ -7,12 +19,30 @@ import md from "node-forge/lib/md.all.js";
  * @return {String}
  */
 export function hash (algo, data, rawOutput = false) {
-  const hash = md[algo].create()
-  hash.update(data, 'utf8')
-  const result = hash.digest()
-  return rawOutput ? result.getBytes() : result.toHex()
+  return hash_wrapper(md[algo], data, rawOutput)
+}
+
+export function hash_md5(data, rawOutput = false)
+{
+  return hash_wrapper(md5, data, rawOutput)
+}
+
+export function hash_sha1(data, rawOutput = false)
+{
+  return hash_wrapper(sha1, data, rawOutput)
+}
+
+export function hash_sha256(data, rawOutput = false)
+{
+  return hash_wrapper(sha256, data, rawOutput)
+}
+
+export function hash_sha512(data, rawOutput = false)
+{
+  return hash_wrapper(sha512, data, rawOutput)
 }
 
 export default {
   hash,
+  hash_md5,
 }
--- test/crypto.test.js
+++ test/crypto.test.js
@@ -1,4 +1,5 @@
 import {base64_decode, base64_encode, random_bytes, random_hex} from '../src/crypto/util.js';
+import {hash, hash_md5, hash_sha1, hash_sha256, hash_sha512} from '../src/crypto/hash.js';
 import test from "ava";
 
 test('base64', t => {
@@ -14,4 +15,37 @@ test('random', t => {
 
   const resultHex = random_hex(12);
   t.is(resultHex.length, 12)
+})
+
+test('hash_all', t => {
+  const text = '123456qweasd请问阿斯顿';
+
+  t.is(hash('md5', text), '9d2f17c9d38caa149155253a73659c5d')
+  t.is(hash('sha1', text), '4577fe4e362cb64712c2c728800e66d38331cfd1')
+  t.is(hash('sha256', text), '43e68d738dcc46b5e944057ac3e5dc60c371af00f18e8ae200c304747d6b0e08')
+  t.is(hash('sha512', text), '3f9556e4b888837a78112f6aac41f093666aa42631232308b31bcaf9fcc6f3137fda8e213065a9223a0992bdffe3ad13ba6737975f954e30ebfe48554e318516')
+})
+
+test('hash_md5', t => {
+  const text = '123456qweasd请问阿斯顿';
+
+  t.is(hash_md5(text), '9d2f17c9d38caa149155253a73659c5d')
+})
+
+test('hash_sha1', t => {
+  const text = '123456qweasd请问阿斯顿';
+
+  t.is(hash_sha1(text), '4577fe4e362cb64712c2c728800e66d38331cfd1')
+})
+
+test('hash_sha256', t => {
+  const text = '123456qweasd请问阿斯顿';
+
+  t.is(hash_sha256(text), '43e68d738dcc46b5e944057ac3e5dc60c371af00f18e8ae200c304747d6b0e08')
+})
+
+test('hash_sha512', t => {
+  const text = '123456qweasd请问阿斯顿';
+
+  t.is(hash_sha512(text), '3f9556e4b888837a78112f6aac41f093666aa42631232308b31bcaf9fcc6f3137fda8e213065a9223a0992bdffe3ad13ba6737975f954e30ebfe48554e318516')
 })

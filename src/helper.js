e82b984ccd8007b02ce43b8738047025fd36ff2b
auooru <auooru@outlook.com>
2021-12-30 16:59:28 +0800

    update: add byteLengthCharCode

--- src/helper.js
+++ src/helper.js
@@ -61,9 +61,27 @@ export function rangeInt (size, startAt = 0) {
   return [...Array(size).keys()].map(i => i + startAt)
 }
 
+/**
+ * @link https://stackoverflow.com/a/23329386
+ * @param {String} str
+ * @return {Number}
+ */
+export function byteLengthCharCode (str) {
+  // returns the byte length of an utf8 string
+  let s = str.length;
+  for (let i = str.length - 1; i >= 0; i--) {
+    const code = str.charCodeAt(i);
+    if (code > 0x7f && code <= 0x7ff) s++
+    else if (code > 0x7ff && code <= 0xffff) s += 2
+    if (code >= 0xDC00 && code <= 0xDFFF) i-- // trail surrogate
+  }
+  return s
+}
+
 export default {
   sleep,
   sleepSeq,
   substrOccurrences,
   rangeInt,
+  byteLengthCharCode,
 }
